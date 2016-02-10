fde2f22429c30710954d0887088b9cb29c62afb1
Derek Laventure <dlaventure@rnao.org>
2016-02-10 16:27:27 -0500

    Adapt remaining pieces of migrate script to match RNAO systems

--- win2k-to-samba4-user-migrate.sh
+++ win2k-to-samba4-user-migrate.sh
@@ -3,21 +3,19 @@ ldapsearch -xb 'dc=rnao,dc=org' -LLL -D "administrator@rnao.org" -W '(&(objectCl
   dn objectClass cn givenName sAMAccountName userAccountControl userPrincipalName description -h 192.168.2.3 > users_win2k3.ldif
 
 # Change DC Name
-sed -i 's/DC=oldserver/DC=newserver,DC=linux/g' users_win2k8.ldif
+sed -i 's/DC=rnao,DC=org/DC=ad,DC=rnao,DC=ca/g' users_win2k3.ldif
 
 # Change User principal domain 
-sed -i  's/@oldserver/@newserver.linux/g' users_win2k8.ldif
+sed -i  's/@rnao.org/@rnao.ca/g' users_win2k3.ldif
 
 # Edit ldif file and remove unwanted accounts administrator and other a/c which you don't want 
 
 # Finally Add all users :) 
-ldapadd -c -h localhost -D CN=Administrator,CN=Users,DC=newserver,DC=LINUX -f users_win2k8.ldif  -W
+ldapadd -c -h localhost -D CN=Administrator,CN=Users,DC=ad,DC=rnao,DC=ca -f users_win2k3.ldif  -W
 
 # Now set all accounts to default password 
-for user in $( grep -oP '(?<=^sAMAccountName:\s).*(=?)' users_win2k8.ldif );
+for user in $( grep -oP '(?<=^sAMAccountName:\s).*(=?)' users_win2k3.ldif );
 do
 	echo "Setting Password for : $user"
 	samba-tool user setpassword --newpassword='pass@123' $user 
 done
-
-#sahbul3U
